151ef101a28f9039972590d97e08d04031c01657
Hammed Balogun <hbalogun@diamondbank.com>
2018-12-19 22:05:50 +0100

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatButtonModule, 
+  MatInputModule,
+  MatCardModule,
+  MatListModule,
+  MatToolbarModule} from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router'
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { QuestionComponent } from './question.component';
+import { ApiService } from './api.service';
+import { QuestionsComponent} from './questions.component'
+import { HomeComponent} from './home.component'
+import { NavComponent} from './nav.component'
+import { QuizComponent} from './quiz.component'
+
+const routes =[
+  { path: '', component: HomeComponent},
+  { path: 'question', component: QuestionComponent},
+  { path: 'questions', component: QuestionsComponent},
+  { path: 'quiz', component: QuizComponent}
+
+]
+@NgModule({
+  declarations: [
+    AppComponent, QuestionComponent,QuestionsComponent,HomeComponent,NavComponent,QuizComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatListModule,
+    HttpClientModule,
+    MatToolbarModule
+  ],
+  providers: [ApiService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
